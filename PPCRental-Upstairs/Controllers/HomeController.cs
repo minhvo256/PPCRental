14d058f0266892934ecad125cd87d3b2f46e0ca2
HaiCao <haicao4@vanlanguni.vn>
2017-12-07 10:42:21 +0700

    Add project files.

---
+++ PPCRental-Upstairs/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PPCRental_Upstairs.Models;
+
+
+namespace PPCRental_Upstairs.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        DemoPPCRentalEntities model = new DemoPPCRentalEntities();
+        public ActionResult Index()
+        {
+            var p = model.PROPERTies.ToList();
+            return View(p);
+        }
+      }
+}
