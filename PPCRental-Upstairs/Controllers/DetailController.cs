6bc1c8c4584a6ef9e3c620c36f0e3d010577501e
minhvo256 <vocongminh256@gmail.com>
2017-12-08 11:37:19 +0700

    View Detail Function

---
+++ PPCRental-Upstairs/Controllers/DetailController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PPCRental_Upstairs.Models;
+namespace PPCRental_Upstairs.Controllers
+{
+    public class DetailController : Controller
+    {
+        DemoPPCRentalEntities m = new DemoPPCRentalEntities();
+        // GET: Detail
+        public ActionResult Detail(int id, string name)
+        {
+            var p = m.PROPERTies.FirstOrDefault(x => x.ID == id || x.PropertyName == name);
+            return View(p);
+        }
+    }
+}
