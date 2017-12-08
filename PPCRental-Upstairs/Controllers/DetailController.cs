5841d013bce3504b901ca75792338542c68772d1
HaiCao <haicao4@vanlanguni.vn>
2017-12-08 15:40:00 +0700

    git commit -a -m "update"

--- PPCRental-Upstairs/Controllers/DetailController.cs
+++ PPCRental-Upstairs/Controllers/DetailController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using PPCRental_Upstairs.Models;
+
 namespace PPCRental_Upstairs.Controllers
 {
     public class DetailController : Controller
--- PPCRental-Upstairs/Controllers/SearchController.cs
+++ PPCRental-Upstairs/Controllers/SearchController.cs
@@ -4,17 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using PPCRental_Upstairs.Models;
+
 namespace PPCRental_Upstairs.Controllers
 {
     public class SearchController : Controller
     {
-        DemoPPCRentalEntities m = new DemoPPCRentalEntities();
+        DemoPPCRentalEntities model = new DemoPPCRentalEntities();
         // GET: Search
-        [HttpPost]
-        public ActionResult Search(string text)
+        public ActionResult Index(string text)
         {
-            var p = m.PROPERTies.ToList().Where(x => x.PropertyName.ToUpper().Contains(text.ToUpper())
+            var p = model.PROPERTies.ToList().Where(x => x.PropertyName.ToUpper().Contains(text.ToUpper())
                || x.ID.ToString().ToUpper().Contains(text.ToUpper()) || x.Content.ToUpper().Contains(text.ToUpper()));
+
             return View(p);
         }
     }
