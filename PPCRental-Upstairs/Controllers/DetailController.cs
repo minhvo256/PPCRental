9d4843728bd842c39ccba987f60368d74fbed190
HaiCao <haicao4@vanlanguni.vn>
2017-12-09 16:16:59 +0700

    update detail

--- PPCRental-Upstairs/Controllers/DetailController.cs
+++ PPCRental-Upstairs/Controllers/DetailController.cs
@@ -14,6 +14,9 @@ namespace PPCRental_Upstairs.Controllers
         public ActionResult Detail(int id, string name)
         {
             var p = m.PROPERTies.FirstOrDefault(x => x.ID == id || x.PropertyName == name);
+            ViewBag.street = m.STREETs.FirstOrDefault(x => x.ID == id);
+            ViewBag.district = m.DISTRICTs.FirstOrDefault(x => x.ID == id);
+            ViewBag.ward = m.WARDs.FirstOrDefault(x => x.ID == id);
             return View(p);
         }
     }
